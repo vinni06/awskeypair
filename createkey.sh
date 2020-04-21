6f0afb6accc62a67e61ab65ee8a31872e9f9dbfa
vinni06 <58682029+vinni06@users.noreply.github.com>
2020-04-21 10:25:06 +0530

    Update createkey.sh

--- createkey.sh
+++ createkey.sh
@@ -7,9 +7,6 @@ i=1
 for var in `cat  $file`;
 do
 echo $var
- # x='.pem'
-  #fname=$var$x
- #echo $fname
 aws ec2 create-key-pair --key-name  $var --query 'KeyMaterial' --output text > $var.pem
     i=$(($i+1))
  if [ $i -gt $n ]
